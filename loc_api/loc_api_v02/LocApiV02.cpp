d28eab452795e0bd5866194a747ca7169955369e
oshmoun <oshmoun100@googlemail.com>
2017-12-30 21:25:35 +0100

    loc_api_v02: LocApiV02: invert wrong NULL check logic
    This NULL check is supposed to guard against initialising with a NULL msgTask, but it does the exact opposite.
    Fix that by inverting the boolean statement.

--- loc_api/loc_api_v02/LocApiV02.cpp
+++ loc_api/loc_api_v02/LocApiV02.cpp
@@ -250,7 +250,7 @@ LocApiBase* LocApiV02::createLocApiV02(const MsgTask *msgTask,
                       LOC_API_ADAPTER_EVENT_MASK_T exMask,
                       ContextBase* context)
 {
-    if (NULL != msgTask) {
+    if (NULL == msgTask) {
         LOC_LOGE("%s:%d]: msgTask can not be NULL", __func__, __LINE__);
         return NULL;
     }
