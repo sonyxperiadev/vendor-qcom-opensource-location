d2e4cc5f06dc2baab006898b5e8e1cb58018188b
oshmoun <oshmoun100@googlemail.com>
2017-12-30 21:48:54 +0100

    Revert "move getAgcInformation and setDefaultReport into GnssAdapter"
    This reverts commit 9954f19d8e2938f92af8c0b4dba37c66d1437a0d.

--- loc_api/loc_api_v02/LocApiV02.h
+++ loc_api/loc_api_v02/LocApiV02.h
@@ -91,6 +91,10 @@ private:
   int convertGnssClock (GnssMeasurementsClock& clock,
       const qmiLocEventGnssSvMeasInfoIndMsgT_v02& gnss_measurement_info);
 
+  /* get AGC information from system status and fill it */
+  static void getAgcInformation(GnssMeasurementsNotification& measurementsNotify,
+      int msInWeek);
+
   /* If Confidence value is less than 68%, then scale the accuracy value to 68%
      confidence.*/
   void scaleAccuracyTo68PercentConfidence(const uint8_t confidenceValue,
