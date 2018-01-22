6d248128b8cbcbc1ea401509f1741c1061efc0db
Alin Jerpelea <alin.jerpelea@sony.com>
2018-01-22 12:40:17 +0100

    use PRODUCT_PLATFORM_SOD as build barrier
    from now on there is no need to manually add a new platform to each
    of the project dependencyes
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+ifeq ($(PRODUCT_PLATFORM_SOD),true)
 LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/build/target_specific_features.mk
 include $(call all-makefiles-under,$(LOCAL_PATH))
