31a0e9f29319e3d2e0ea10875db1e5a264072cfd
Alexander Diewald <Diewi@diewald-net.com>
2018-01-31 09:55:28 +0100

    sony-location: Add rhine, shinano to build guard.
    * We are still alive and blocked since bb8976e030f679882719ae35b7674c83c16950a8
    
    Change-Id: I7e2f24ed71bd2de9e9dcab3853b2cfcd51134f33

--- Android.mk
+++ Android.mk
@@ -5,3 +5,7 @@ include $(LOCAL_PATH)/build/target_specific_features.mk
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
 endif
+
+ifneq ($(filter rhine shinano,$(PRODUCT_PLATFORM)),)
+include $(call all-subdir-makefiles)
+endif
