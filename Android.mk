00490cfae551bf553d93118e7a9358a8592dcc34
David <Davidteri91@gmail.com>
2018-10-25 13:52:39 +0200

    Location: add kernel version guard
    for custom projects using kernel 4.4 and 4.9 at same time
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 ifeq ($(PRODUCT_PLATFORM_SOD),true)
+ifeq ($(TARGET_KERNEL_VERSION), 4.9)
 LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/build/target_specific_features.mk
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
 endif
