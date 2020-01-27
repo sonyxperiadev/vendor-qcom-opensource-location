4e539f202afdef75183c6b3de9cb25f483d0b6a8
Alin Jerpelea <alin.jerpelea@sony.com>
2020-01-27 09:37:57 +0100

    add the 4.9 kernel to the build barrier
    The legacy devices use kernel 4.9 on Android 10
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 ifeq ($(PRODUCT_PLATFORM_SOD),true)
-ifeq ($(TARGET_KERNEL_VERSION), 4.14)
+ifneq (,$(filter $(strip $(SOMC_KERNEL_VERSION)),4.9 4.14))
 LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/build/target_specific_features.mk
 include $(call all-makefiles-under,$(LOCAL_PATH))
