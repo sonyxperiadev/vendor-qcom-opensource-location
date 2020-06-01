dde15e17fdab535a310c81bf63b113c12066e5ee
ix5 <ix5@users.noreply.github.com>
2020-06-02 00:02:52 +0200

    libloc_loader: Use exported includes
    Separate header libraries are not really needed when all
    users of the lib already link against it.
    Use LOCAL_EXPORT_C_INCLUDE_DIRS instead.
    
    Same goes for libloc_api_qmi_headers. Any module that needs
    them also has to pull in libloc_loader to actually dlopen
    the proprietary libs that are references from the headers.
    
    Just make it easier for everyone and provide the stub
    headers to every module that links against libloc_loader.
    
    In the process, get rid of the unused dsi_netctrl.h.

--- loc_api/libloc_loader/Android.mk
+++ loc_api/libloc_loader/Android.mk
@@ -1,11 +1,9 @@
-ifneq ($(BUILD_TINY_ANDROID),true)
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libloc_loader
-
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SHARED_LIBRARIES := \
@@ -16,19 +14,19 @@ LOCAL_SRC_FILES += \
 
 LOCAL_HEADER_LIBRARIES := libcutils_headers libutils_headers
 
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/include
+# Make headers under include/ available to all modules
+# that link to libloc_loader:
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+
+# 4.9 targets require libqsocket.so and ipc headers
 ifeq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),3.18 4.4 4.9))
 LOCAL_CFLAGS += -DUSE_QSOCKET
+# For UAPI <linux/msm_ipc.h>
 LOCAL_HEADER_LIBRARIES += \
     device_kernel_headers
 endif
 
-LOCAL_PROPRIETARY_MODULE := true
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libloc_loader_headers
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
-include $(BUILD_HEADER_LIBRARY)
-
 include $(BUILD_SHARED_LIBRARY)
 
-endif # not BUILD_TINY_ANDROID
