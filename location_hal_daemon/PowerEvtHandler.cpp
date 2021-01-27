b16b48a3b60b9b8fda97975c419d253b2a425bd5
Wei (GPS SD) Chen <weic@codeaurora.org>
2021-01-27 11:32:00 -0800

    Revert "Location hal daemon: wait for 1000 msec before ack to power manager"
    Make hal daemon ack back to power manager daemon within the
    required timeout of 350ms
    
    This reverts commit 2b4ca9bdf0652b849f94e257a542da0f0f0a185d.
    
    CRs-fixed: 2864512
    
    Change-Id: I039c1a0378f97323795b3da0336febb53a667783

--- location_hal_daemon/PowerEvtHandler.cpp
+++ location_hal_daemon/PowerEvtHandler.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018, 2020 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018 - 2021 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -32,7 +32,7 @@
 #include <log_util.h>
 #include <syslog.h>
 
-#define ACK_TIMEOUT_US 1000000 // 1000 msec
+#define ACK_TIMEOUT_US 300000 // 300 msec
 
 LocationApiService* PowerEvtHandler::mLocationApiService = nullptr;
 
