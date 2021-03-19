6b05221d79628642b1317e1a46571e254997623d
Harikrishnan Hariharan <hahariha@codeaurora.org>
2021-03-19 21:46:15 +0530

    Fix KW issue in client api testapp
    Change-Id: Id9a52faa6993641c41cb2d62362c99392dfebf01
    CRs-Fixed: 2910370

--- client_api_testapp/main.cpp
+++ client_api_testapp/main.cpp
@@ -100,7 +100,7 @@ static int autoTestTimeoutSec = 0x7FFFFFFF;
 
 // debug utility
 static uint64_t getTimestampMs() {
-    struct timespec ts;
+    struct timespec ts = {};
     clock_gettime(CLOCK_BOOTTIME, &ts);
     uint64_t msec =
             ((uint64_t)(ts.tv_sec)) * 1000ULL + ((uint64_t)(ts.tv_nsec)) / 1000000ULL;
