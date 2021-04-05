c99d180a76045410732889f605b599abf43acb50
Dante Russo <drusso@codeaurora.org>
2021-04-05 14:45:41 -0700

    Add libloc_socket to product packages
    Allows libloc_socket to be built by default
    
    Change-Id: I619596aeec13ad9eafa0128a19f5abc5ef101fe9
    CRs-fixed: 2916461

--- gps_vendor_product.mk
+++ gps_vendor_product.mk
@@ -31,6 +31,7 @@ PRODUCT_PACKAGES += libgnsspps
 PRODUCT_PACKAGES += libsynergy_loc_api
 PRODUCT_PACKAGES += izat_remote_api_headers
 PRODUCT_PACKAGES += loc_sll_if_headers
+PRODUCT_PACKAGES += libloc_socket
 
 endif #is-board-platform-in-list
 
