2792b4d5774e5322af9c842464f9a67829a4d1fd
Sumit Kumar <quic_sumitku@quicinc.com>
2022-01-24 18:26:09 +0530

    Enable protected dynamic interface control.
    Changes to Enebale protected dynamic interface control
    Added support to
    1. Get the engine lock state.
    2. Handle engine lock state event.
    3. Store all the sessions when the engine is locked.
    4. Restore all the sessions when the engine unlock event is received.
    
    Change-Id: I14b88114eee1a4e3cf54ea460ec5e828bfe693bc
    CRs-fixed: 3130222

--- loc_api/loc_api_v02/LocApiV02.cpp
+++ loc_api/loc_api_v02/LocApiV02.cpp
@@ -516,6 +516,8 @@ LocApiV02 :: open(LOC_API_ADAPTER_EVENT_MASK_T mask)
        mContext->setEngineCapabilities(supportedMsgList,
             (getSupportedFeatureList_ind.feature_len != 0 ? getSupportedFeatureList_ind.feature:
             NULL), gnssMeasurementSupported);
+
+        getEngineLockStateSync();
     }
   }
 
@@ -2479,6 +2481,18 @@ GnssConfigGpsLock LocApiV02::convertGpsLockFromQMItoAPI(qmiLocLockEnumT_v02 lock
       case eQMI_LOC_LOCK_NONE_V02:
       default:
         return GNSS_CONFIG_GPS_LOCK_NONE;
+    }
+}
+
+EngineLockState LocApiV02::convertEngineLockState(qmiLocEngineLockStateEnumT_v02 LockState)
+{
+    switch (LockState) {
+      case eQMI_LOC_ENGINE_LOCK_STATE_ENABLED_V02:
+        return ENGINE_LOCK_STATE_ENABLED;
+      case eQMI_LOC_ENGINE_LOCK_STATE_DISABLED_V02:
+        return ENGINE_LOCK_STATE_DISABLED;
+      default:
+        return ENGINE_LOCK_STATE_INVALID;
     }
 }
 
@@ -6887,6 +6901,10 @@ void LocApiV02 :: eventCb(locClientHandleType /*clientHandle*/,
       reportLatencyInfo(eventPayload.pLocLatencyInfoIndMsg);
       break;
 
+    case QMI_LOC_EVENT_ENGINE_LOCK_STATE_IND_V02:
+        LOC_LOGd("Got QMI_LOC_EVENT_ENGINE_STATE_IND_V02");
+        reportEngineLockStatus(eventPayload.pEngineLockStateIndMsg->engineLockState);
+        break;
   }
 }
 
@@ -6969,6 +6987,15 @@ LocationError LocApiV02 :: setGpsLockSync(GnssConfigGpsLock lock)
             loc_get_v02_client_status_name(status),
             loc_get_v02_qmi_status_name(setEngineLockInd.status));
         err = LOCATION_ERROR_GENERAL_FAILURE;
+    } else {
+        if (setEngineLockReq.lockType == eQMI_LOC_LOCK_NONE_V02 ||
+                setEngineLockReq.lockType == eQMI_LOC_LOCK_MT_V02) {
+            reportEngineLockStatus(eQMI_LOC_ENGINE_LOCK_STATE_ENABLED_V02);
+            setEngineLockState(ENGINE_LOCK_STATE_ENABLED);
+        } else {
+            reportEngineLockStatus(eQMI_LOC_ENGINE_LOCK_STATE_DISABLED_V02);
+            setEngineLockState(ENGINE_LOCK_STATE_DISABLED);
+        }
     }
     return err;
 }
@@ -7031,6 +7058,41 @@ int LocApiV02 :: getGpsLock(uint8_t subType)
         }
     }
     return ret;
+}
+
+void LocApiV02 :: getEngineLockStateSync() {
+
+    qmiLocGetEngineLockReqMsgT_v02 getEngineLockReq;
+    qmiLocGetEngineLockIndMsgT_v02 getEngineLockInd;
+    locClientStatusEnumType status;
+    locClientReqUnionType req_union;
+
+    memset(&getEngineLockInd, 0, sizeof(getEngineLockInd));
+
+    /*Passing req_union as a parameter even though this request has no payload
+    since NULL or 0 gives an error during compilation*/
+    getEngineLockReq.subType_valid = true;
+    getEngineLockReq.subType = eQMI_LOC_LOCK_ALL_SUB_V02;
+    req_union.pGetEngineLockReq = &getEngineLockReq;
+
+    status = locSyncSendReq(QMI_LOC_GET_ENGINE_LOCK_REQ_V02,
+                            req_union, LOC_ENGINE_SYNC_REQUEST_TIMEOUT,
+                            QMI_LOC_GET_ENGINE_LOCK_IND_V02,
+                            &getEngineLockInd);
+
+    if (status != eLOC_CLIENT_SUCCESS || getEngineLockInd.status != eQMI_LOC_SUCCESS_V02) {
+        LOC_LOGE("%s:%d]: Get engine lock failed. status: %s, ind status:%s\n",
+                 __func__, __LINE__,
+                 loc_get_v02_client_status_name(status),
+                 loc_get_v02_qmi_status_name(getEngineLockInd.status));
+    } else {
+        if (getEngineLockInd.lockType == eQMI_LOC_LOCK_NONE_V02 ||
+                getEngineLockInd.lockType == eQMI_LOC_LOCK_MT_V02) {
+            setEngineLockState(ENGINE_LOCK_STATE_ENABLED);
+        } else {
+            setEngineLockState(ENGINE_LOCK_STATE_DISABLED);
+        }
+    }
 }
 
 LocationError
@@ -7372,6 +7434,17 @@ void LocApiV02::reportLatencyInfo(const qmiLocLatencyInformationIndMsgT_v02* pLo
              gnssLatencyInfo.hlosQtimer1, gnssLatencyInfo.hlosQtimer2);
 
     LocApiBase::reportLatencyInfo(gnssLatencyInfo);
+}
+
+void LocApiV02::reportEngineLockStatus(const qmiLocEngineLockStateEnumT_v02 engineLockState)
+{
+    EngineLockState lockState = convertEngineLockState(engineLockState);
+    LOC_LOGd("Lock State old %d new %d", getEngineLockState(), lockState);
+
+    if (lockState != getEngineLockState() && ENGINE_LOCK_STATE_INVALID != lockState ) {
+        setEngineLockState(lockState);
+        LocApiBase::reportEngineLockStatus(lockState);
+    }
 }
 
 void LocApiV02::configRobustLocation
@@ -10076,7 +10149,11 @@ LocApiV02::startBatching(uint32_t sessionId,
 
     if (!rv) {
         LOC_LOGE("%s] failed!", __func__);
-        err = LOCATION_ERROR_GENERAL_FAILURE;
+        if (eQMI_LOC_GNSS_DISABLED_V02 == ind.status) {
+            err = LOCATION_ERROR_GNSS_DISABLED;
+        } else {
+            err = LOCATION_ERROR_GENERAL_FAILURE;
+        }
     }
 
     if (adapterResponse != NULL) {
